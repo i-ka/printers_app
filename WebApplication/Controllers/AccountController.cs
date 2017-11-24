b1d0cc924da64c589fddf2f8cb617710c5776eaf
i-ka <i-ka2010@mail.ru>
2017-11-24 11:32:43 +0500

    Remove usings

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebApplication.Data;
 using WebApplication.Data.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using WebApplication.Data.Models;
